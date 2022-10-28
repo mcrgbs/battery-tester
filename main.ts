0bd5efb08f18e7035f0d2f0a35ddd12e2c5cbc8b
mcrgbs <116827514+mcrgbs@users.noreply.github.com>
2022-10-28 18:44:26 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,19 +2,13 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(pins.analogReadPin(AnalogPin.P0))
 })
 input.onButtonPressed(Button.B, function () {
-    millivolt = pins.map(
-    pins.analogReadPin(AnalogPin.P0),
-    0,
-    1023,
-    0,
-    3000
-    )
-    basic.showNumber(millivolt)
-    if (millivolt < 1200) {
+    millivolt = pins.analogReadPin(AnalogPin.P0)
+    if (millivolt == millivolt * (1000 / 340)) {
         music.startMelody(music.builtInMelody(Melodies.Wawawawaa), MelodyOptions.Once)
     } else {
         music.startMelody(music.builtInMelody(Melodies.BaDing), MelodyOptions.Once)
     }
+    basic.showNumber(millivolt)
 })
 let millivolt = 0
 basic.showString("Volts")
